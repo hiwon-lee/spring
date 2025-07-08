4582be80a6b35f72212c50c3d9942bd0e363cfb2
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-07-08 17:03:02 +0900

    refactor : JPQL -> JPA Criteria

--- src/main/java/jpabook/jpashop/domain/OrderSearch.java
+++ src/main/java/jpabook/jpashop/domain/OrderSearch.java
@@ -1,9 +1,19 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+
 public class OrderSearch {
 
+
+
+    @Getter
+
     private String memberName;  // 회원 이름
+
+
+    @Getter
     private OrderStatus orderStatus;    // 주문 상태[ORDER, CANCEL]
 
     // Getter, Setter
+
 }
