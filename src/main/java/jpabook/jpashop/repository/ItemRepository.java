f30715dba25445546fbb344d684ce978123c0451
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-07-10 13:53:15 +0900

    feat : 상품 등록 및 수정 기능 구현

--- src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -8,18 +9,21 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
 public class ItemRepository {
 
-    private final EntityManager em;
+    @PersistenceContext
+    EntityManager em;
 
+    // 신규 데이터 저장뿐만 아니라 변경된 데이터의 저장이라는 의미도 포함한다.
+    // 이렇게 함으로써 이 메서드를 사용하는 클라이언트는 저장과 수정을 구분하지 않아도 되므로
+    // 클라이언트의 로직이 단순해진다.
     public void save(Item item){
         // id가 신규면 새롭게 저장
         if (item.getId() == null) {
-            em.persist(item);
+            em.persist(item);  // 새로운 엔티티로 판단해 영속화
         } else {
             // 있으면 저장된 엔티티를 수정한다고 보고 실행
-            em.merge(item);
+            em.merge(item);  // 식별자가 있는 경우(준영속 상태인 경우) 병합
         }
     }
 
